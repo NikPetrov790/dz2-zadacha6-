6f0e0b57aa3a0736356add2bde82e9e48a8c59cc
Petrov Nikolay <nikolay.petrov99@mail.ru>
2022-08-06 12:42:10 +0300

    Zadacha6

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 1)
+{
+    Console.WriteLine("Значение"+ number+ "является нечетным");
+}
+else
+{
+    Console.WriteLine("Значение"+ number+ "является четным");
+}
